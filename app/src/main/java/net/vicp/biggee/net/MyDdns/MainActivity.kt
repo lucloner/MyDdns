3033f4893c256b15b370c1933f7c3ec7efed56e0
Biggee <lucloner@hotmail.com>
2019-03-27 11:49:45 +0800

    添加了安全配置并隐藏了密码 完成

--- app/src/main/java/net/vicp/biggee/net/MyDdns/MainActivity.kt
+++ app/src/main/java/net/vicp/biggee/net/MyDdns/MainActivity.kt
@@ -1,20 +1,16 @@
 package net.vicp.biggee.net.MyDdns
 
 import android.os.Bundle
-import android.os.Handler
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import okhttp3.*
-import okhttp3.EventListener
-import java.io.BufferedReader
-import java.io.File
-import java.io.InputStreamReader
-import java.net.HttpURLConnection
-import java.net.URL
-import java.net.URLEncoder
+import okhttp3.Cache
+import okhttp3.Credentials
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import java.text.SimpleDateFormat
 import java.util.*
 import java.util.concurrent.TimeUnit
 
@@ -28,17 +24,15 @@ class MainActivity : AppCompatActivity() {
         val d = findViewById<EditText>(R.id.ddns)
         val u = findViewById<EditText>(R.id.username)
         val p = findViewById<EditText>(R.id.password)
+        runOnUiThread {
+            p.setText(BuildConfig.myPass)
+        }
         val l = findViewById<TextView>(R.id.logView)
-        val s = "http://${u.text}:${p.text}@ddns.oray.com/ph/update?hostname=${d.text}"
-        val e = Base64.getEncoder().encodeToString("${u.text}:${p.text}".toByteArray())
-        val url = URL(s)
-        val param="hostname=${d.text}"
+        val s = "http://ddns.oray.com/ph/update?hostname=${d.text}"
         val a=Credentials.basic(u.text.toString(),p.text.toString())
-//        Comparator<String> { obj, anotherString -> obj.compareTo(anotherString) }
-
         val r=Request.Builder().apply {
             get()
-            url("http://ddns.oray.com/ph/update?hostname=${d.text}")
+            url(s)
             header("Authorization",a)
         }.build()
 
@@ -48,11 +42,9 @@ class MainActivity : AppCompatActivity() {
                     readTimeout(5,TimeUnit.SECONDS)
                     cache(Cache(cacheDir,Int.MAX_VALUE.toLong()))
                     }.build()
-
                 val res=c.newCall(r).execute()
-
                 runOnUiThread{
-                    l.text="${System.currentTimeMillis()}\n"
+                    l.text = "${SimpleDateFormat.getDateTimeInstance().format(Date())}\n"
                     l.append(res.body()!!.string())
                 }
             }.start()
